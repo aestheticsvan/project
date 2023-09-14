8347e78b85243f9aa432b5abdc97a9430b4c64b0
aestheticsvan <x888xx8@icloud.com>
2023-09-15 02:19:21 +0800

    debug false

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'НОВОСТИ',
       theme: ThemeData(
        scaffoldBackgroundColor: Colors.white,
