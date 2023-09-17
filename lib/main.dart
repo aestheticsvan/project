fdbc610cdc929af7f2c59b687729082034b0f8f2
aestheticsvan <x888xx8@icloud.com>
2023-09-17 11:22:53 +0800

    card, icons

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'НОВОСТИ',
       theme: ThemeData(
-       scaffoldBackgroundColor: Colors.white54,
+       scaffoldBackgroundColor: Color.fromARGB(255, 219, 219, 219),
        primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Новости'),
@@ -34,13 +34,14 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   @override
-  List<News> _news = [
+  List<News> _news = <News>[
     News('Тодд Говард: "А мы Starfield выпустили"', 'Студия Immortals of Aveum уволила почти половину сотрудников через несколько недель после релиза. Компания Ascendant Studios, создавшая игру Immortals of Aveum, сократила почти половину своего штата, сообщили три сотрудника студии. По оценкам сотрудников, до увольнения в студии работало от 80 до 100 человек, а сокращено было около 40. Генеральный директор Ascendant Брет Роббинс (Bret Robbins) объявил об увольнениях на собрании в четверг.', DateTime.now(), 'https://i.playground.ru/p/dZKnhZMm5eKwNm5914JT8w.jpeg'),
     News('Remedy показала, как изменился хоррор Alan Wake 2 за три года разработки" ', 'На The Game Awards в 2021 году Remedy Entertainment официально анонсировала долгожданное продолжение психологического триллера Alan Wake. Сиквел пережил тернистый путь разработки и несколько трансформаций, которые в итоге сделали его полноценным хоррором с элементами мистики, детектива и выживания. Авторы Alan Wake 2 наглядно показали, как изменилась игра за три года активной разработки.', DateTime.now(), 'https://i.playground.ru/p/uil--74pW6y3NHMFm8uw5Q.jpeg'),
     News('Сиквел Rust не будет разрабатываться на Unity ', 'Гэрри Ньюман, основатель студии Facepunch, известной по Rust, высказал свое мнение о недавних изменениях в Unity, в частности о грядущей системе "налога за установку" Вкратце, Ньюман раскритиковал этот выбор и подтвердил, что Rust 2 не будет разрабатываться на Unity. Интересно то, что Rust 2 еще не была официально представлена или подтверждена, поэтому слова Ньюмана, по сути, являются анонсом' , DateTime.now(), 'https://i.playground.ru/p/vHUzNvoZj9p0xz-tR_7ALw.jpeg'),
     News('Sony present information', 'Sony Interactive Entertainment только что объявила о новой презентации State of Play, сообщив, что она запланирована на пятницу, 15 сентября 2023 года, в 00:00 по московскому времени. Японский гигант сделал это важное объявление в сообщении в блоге PlayStation, а также поделился следующим сообщением:', DateTime.now(), 'https://blog.ja.playstation.com/tachyon/sites/7/2060/09/b90cc9b5a1773d60f8dfe711e454ec9491324d88.png?resize=1088%2C612&crop_strategy=smart&zoom=1.5'),
+  
   ];
-List<String> _month = [
+List<String> _month = <String>[
   'Jan',
   'Feb',
   'Mar',
@@ -60,35 +61,42 @@ List<String> _month = [
 
     _newsList.forEach((element) {
       _newsRows.add(
-        Card(
-          child: Column(
-            children: [
-              Container(
-                child: element.imageUrl.isEmpty?Container():Image.network(element.imageUrl),
-                height: 250, width: 350,
-                color: Colors.white,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 0, 0, 0),
-                child: ListTile(
-                title: Text(element.name, style: TextStyle(fontWeight: FontWeight.bold),),
-                subtitle: Text(element.body, style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.w300),),
-                trailing: IconButton(icon: Icon(Icons.keyboard_arrow_right, size: 30, color: Colors.red,),onPressed: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=>  NewsDescription(news: element)));
-                },),
-                    ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(Icons.favorite, size: 30, color: Colors.red,),
-                    Text('${element.date.day}, ${_month[element.date.month-1]}', style: TextStyle(fontStyle: FontStyle.italic),)
-                  ],
+        Padding(
+          padding: const EdgeInsets.fromLTRB(4,5,4,15),
+          child: 
+          Card(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5,15,5,15),
+                  child: Container(
+                    child: element.imageUrl.isEmpty?Container():Image.network(element.imageUrl),
+                    height: 255, width: 350,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), color: const Color.fromARGB(255, 219, 219, 219)), //цвет за картинкой 
+                  ),
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 0, 0, 0),
+                  child: ListTile(
+                  title: Text(element.name, style: TextStyle(fontWeight: FontWeight.bold),),
+                  subtitle: Text(element.body, style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.w300),),
+                  trailing: IconButton(icon: Icon(Icons.keyboard_arrow_right, size: 30, color: Colors.red,),onPressed: (){ //кнопка перехода на новость
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>  NewsDescription(news: element)));
+                  },),
+                      ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.favorite, size: 30, color: Colors.red,),
+                      Text('${element.date.day}, ${_month[element.date.month-1]}', style: TextStyle(fontStyle: FontStyle.italic),)
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ));
      });
@@ -103,6 +111,7 @@ List<String> _month = [
         ),
         
         body: ListView(
+          //scrollDirection: Axis.horizontal, ?
           children: _buildNewsList(_news),
         )
      );
--- lib/news_description.dart
+++ lib/news_description.dart
@@ -7,39 +7,45 @@ class NewsDescription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.fromARGB(255, 219 , 219, 219),
+
       appBar: AppBar(title: Text(news.name),
       ),
-      body: Center(child: Column(children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: news.imageUrl.isEmpty?Container(height: 200, color: Colors.amber,):Image.network(news.imageUrl),
-        ),
-        Row(
-          children: [
-            Flexible(
-              fit: FlexFit.loose,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text('${news.body}'),
-              )),
-          ],
-        ),
-         const Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(30,5,30 ,0 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(Icons.favorite , color: Colors.red,),
-                    Icon(Icons.comment , color: Colors.blueGrey,),
-                  ],
-                ),
-              )
-            ],
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10,15,10,240),
+        child: Card(
+          child: Center(child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: news.imageUrl.isEmpty?Container(height: 200, color: Colors.amber,):Image.network(news.imageUrl),
+            ),
+            Row(
+              children: [
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text('${news.body}'),
+                  )),
+              ],
+            ),
+             const Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(30,5,30 ,0 ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.favorite , color: Colors.red,),
+                        Icon(Icons.comment , color: Colors.blueGrey,),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+          ],)
           ),
-      ],)
+        ),
       ),
     );
   }
