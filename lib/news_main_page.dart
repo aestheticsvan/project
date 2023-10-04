19711816ce5bf5b012cf83942a84057bc42315d4
aestheticsvan <x888xx8@icloud.com>
2023-10-04 14:58:54 +0800

    1 commit

--- lib/news_main_page.dart
+++ lib/news_main_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_final_fields
 
 import 'package:flutter/material.dart';
 import 'package:project/add_post.dart';
